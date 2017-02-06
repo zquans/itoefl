adbd33c1e6dc74406b801e9683a0eb517d52bdc8
lebang08 <443664360@qq.com>
2017-02-06 13:22:17 +0800

    音频变速播放/文件下载解压路径
    1.音频初步方案
    2.文件存储路径及方式确定

--- src/main/java/com/iyuce/itoefl/UI/Listening/Activity/DoResultActivity.java
+++ src/main/java/com/iyuce/itoefl/UI/Listening/Activity/DoResultActivity.java
@@ -1,16 +1,27 @@
 package com.iyuce.itoefl.UI.Listening.Activity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 
 import com.iyuce.itoefl.R;
+import com.iyuce.itoefl.Utils.LogUtil;
+
+import java.io.File;
+import java.io.IOException;
 
 public class DoResultActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageButton mImgBtnPlay;
 
+    private MediaPlayer mMediaPlayer;
+    private SoundPool mSoundPool;
     private boolean isPlay = true;
 
     @Override
@@ -27,6 +38,9 @@ public class DoResultActivity extends AppCompatActivity implements View.OnClickL
 
         mImgBtnPlay = (ImageButton) findViewById(R.id.imgbtn_activity_do_result_play);
         mImgBtnPlay.setOnClickListener(this);
+
+        mSoundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        mMediaPlayer = new MediaPlayer();
     }
 
     @Override
@@ -41,6 +55,22 @@ public class DoResultActivity extends AppCompatActivity implements View.OnClickL
                 else
                     mImgBtnPlay.setBackgroundResource(R.mipmap.icon_media_play);
                 isPlay = !isPlay;
+
+                //MediaPlayer
+                try {
+                    mMediaPlayer.setDataSource("/storage/emulated/0/download/le/16895.mp3");
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        mMediaPlayer.setPlaybackParams(mMediaPlayer.getPlaybackParams().setSpeed(2.0f));
+                    }
+                    mMediaPlayer.prepare();
+                    mMediaPlayer.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //SoundPool
+//                mSoundPool.load("/storage/emulated/0/download/le/16895.mp3", 1);
+//                mSoundPool.play(1, 1, 1, 0, 0, 1.5f);
                 break;
         }
     }
--- src/main/java/com/iyuce/itoefl/UI/Listening/Activity/TopListeneringPageActivity.java
+++ src/main/java/com/iyuce/itoefl/UI/Listening/Activity/TopListeneringPageActivity.java
@@ -1,6 +1,7 @@
 package com.iyuce.itoefl.UI.Listening.Activity;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -26,12 +27,31 @@ public class TopListeneringPageActivity extends AppCompatActivity implements Vie
     private RecyclerView.Adapter mAdapter;
     private ArrayList<String> mDataList = new ArrayList<>();
 
+    private String mSavePath, mSQLitePath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_listenering_page);
 
         initView();
+        initFileDir();
+    }
+
+    /**
+     * 初始化下载目标文件夹
+     */
+    private void initFileDir() {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            // sd卡根目录
+            String sdPath = Environment.getExternalStorageDirectory() + "/";
+            mSavePath = sdPath + "Itoefl";
+            //建立目标路径文件
+            File dir = new File(mSavePath);
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+        }
     }
 
     private void initView() {
@@ -51,44 +71,47 @@ public class TopListeneringPageActivity extends AppCompatActivity implements Vie
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.txt_header_title_menu:
-                OkGo.get("http://img.enhance.cn/toefl/zip/listenaudiozip/1402.zip")
-                        .execute(new FileCallback() {
-                            @Override
-                            public void downloadProgress(long currentSize, long totalSize, float progress, long networkSpeed) {
-                                super.downloadProgress(currentSize, totalSize, progress, networkSpeed);
-                                LogUtil.i(currentSize + "||" + totalSize + "||" + progress);
-                            }
-
-                            @Override
-                            public void onSuccess(File file, Call call, Response response) {
-                                List<File> mList;
-                                LogUtil.i(file.getAbsolutePath() + "||" + file.getName() + "||" + file.toString());
-                                //TODO Zip解压相关
-                                try {
-                                    //获取文件的文件名
-                                    mList = ZipUtil.GetFileList("/storage/emulated/0/download/1402.zip", true, true);
-                                    for (int i = 0; i < mList.size(); i++) {
-                                        LogUtil.i("mList = " + mList.get(i).getName());
-                                    }
-                                    //解压zip到对应路径
-                                    ZipUtil.UnZipFolder("/storage/emulated/0/download/1402.zip", "/storage/emulated/0/download/le");
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                                //TODO 打开对应的SQLite数据库
-                                //获取info.db的路径
-                            }
-
-                            @Override
-                            public void onError(Call call, Response response, Exception e) {
-                                super.onError(call, response, e);
-                                LogUtil.i("onError = " + response.toString());
-                            }
-                        });
+                doDownLoad();
                 break;
             case R.id.imgbtn_header_title:
                 finish();
                 break;
         }
+    }
+
+    private void doDownLoad() {
+        OkGo.get("http://img.enhance.cn/toefl/zip/listenaudiozip/1402.zip")
+                .execute(new FileCallback(mSavePath, "") {
+                    @Override
+                    public void downloadProgress(long currentSize, long totalSize, float progress, long networkSpeed) {
+                        super.downloadProgress(currentSize, totalSize, progress, networkSpeed);
+                        LogUtil.i(currentSize + "||" + totalSize + "||" + progress);
+                    }
+
+                    @Override
+                    public void onSuccess(File file, Call call, Response response) {
+                        /**Zip解压文件夹*/
+                        List<File> mList;
+                        try {
+                            //获取文件的文件名
+                            //mList = ZipUtil.GetFileList("/storage/emulated/0/download/1402.zip", true, true);
+                            mList = ZipUtil.GetFileList(file.getAbsolutePath(), true, true);
+                            for (int i = 0; i < mList.size(); i++) {
+                                if (mList.get(i).getName().contains("sqlite")) {
+                                    //拿出数据库文件的路径
+                                    mSQLitePath = mSavePath + "/" + mList.get(i).getName();
+                                }
+                                LogUtil.i("mList = " + mList.get(i).getName());
+                            }
+                            //解压zip到对应路径
+                            //ZipUtil.UnZipFolder("/storage/emulated/0/download/1402.zip", "/storage/emulated/0/download/le");
+                            ZipUtil.UnZipFolder(file.getAbsolutePath(), mSavePath);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        //输出对应的SQLite数据库路径
+                        LogUtil.i("mSQLitePath = " + mSQLitePath);
+                    }
+                });
     }
 }
